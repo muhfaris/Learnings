91a2111ea0dde4e0b6c5df78caf0c8e69c509f4b
Faris Afif <maillfaris@gmail.com>
2017-09-22 08:31:02 +0700

    change name python

---
+++ Python/Python/Basic/step_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+##
+# Learning - 1
+# I'm learning with my self, so i'm sorry if my language not good.
+# this case one, python will wait for user to type something (followed 
+# Enter).
+# command  : 
+# using python 2 use raw_input()
+# using python 3 use input()
+##
+
+print("Learning-1") #print text "Learning -1"
+print("Case- function input") #print text "Case- function input"
+name = input("Whats Your Name ?? : ") #function assign type something from user
+print("My name is",name) #text print text and variabel of name
+
+###output 
+#Learning-1
+#Case- function input
+#Whats Your Name ?? : Faris
+#My name is Faris
+###
---
+++ Python/Python/Basic/step_8.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+###
+# Learning -8 
+###
+# function while 
+
+# like for
+####
+
+print("Learning-8")
+print("Loop data with while")
+print("")
+loop = 1 #value variaebl loop is 1
+while loop<=10: #start loop from 1 - 10
+	print("Loop -",loop) # display number loop
+	loop = loop+1 #loop + 1 to next loop
+print("")
